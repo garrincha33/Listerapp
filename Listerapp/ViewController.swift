6236f66c72df3b25f9812fc57143f90805c68257
Richard Price <mrspondooli@gmail.com>
2016-10-30 12:52:04 +0000

    Initial Commit

---
+++ Listerapp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Listerapp
+//
+//  Created by Richard Price on 30/10/2016.
+//  Copyright © 2016 twisted echo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
